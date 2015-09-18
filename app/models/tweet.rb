1fcac15a8c4daa55f983bfe002a7dad6b58350d4
MrDhillon <harman.singh.dhillon@gmail.com>
2015-09-18 19:32:39 -0400

    error showing for duplicate tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,7 @@
 class Tweet < ActiveRecord::Base
     validates :tweet, presence: true
     validates :post_time, presence: true
-    # validate :check_for_duplicate_tweets
+    validate :check_for_duplicate_tweets
 
     def check_for_duplicate_tweets
       if  Tweet.where(tweet: tweet) == []
