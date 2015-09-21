af4b7207704fa5625cd03c7ca6d5f29a22b9b8af
samkessaram <samkessaram@gmail.com>
2015-09-21 21:56:40 +0000

    drop add

--- db/migrate/20150917203408_change_datetype.rb
+++ db/migrate/20150917203408_change_datetype.rb
@@ -1,5 +1,6 @@
 class ChangeDatetype < ActiveRecord::Migration
   def change
-    change_column :tweets, :post_time, :datetime
+    remove_column :tweets, :post_time
+    add_column :tweets, :post_time, :datetime
   end
 end
