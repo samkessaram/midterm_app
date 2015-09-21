94ddeaa4fc5f85f3a15d83153b0bed988a4b2ec1
samkessaram <samkessaram@gmail.com>
2015-09-21 19:57:15 +0000

    changed timezone for db config

--- config/database.rb
+++ config/database.rb
@@ -12,12 +12,14 @@ configure do
   else
     set :database, ENV['DATABASE_URL']
   end
-  
+
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
   Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
     filename = File.basename(model_file).gsub('.rb', '')
     autoload ActiveSupport::Inflector.camelize(filename), model_file
   end
+
+  Time.zone = "UTC"
 
 end
